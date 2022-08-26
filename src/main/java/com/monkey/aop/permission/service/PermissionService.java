2ec40af66900461ffc2bde6c289c20890d1bd2d5
dlwlgh9412 <dlwlgh9412@gmail.com>
2022-08-26 17:06:36 +0900

    no message

--- src/main/java/com/monkey/aop/permission/service/PermissionService.java
+++ src/main/java/com/monkey/aop/permission/service/PermissionService.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PermissionService {
-    public void checkPermission(UserId userId, PermissionEntity permissionElement) {
-        if (userId.getId() != permissionElement.getUserId().getId())
+    public void checkPermission(UserId userId, PermissionEntity permissionEntity) {
+        if (userId.getId() != permissionEntity.getUserId().getId())
             throw new MonkeyException(ErrorCode.E001, HttpStatus.UNAUTHORIZED);
     }
 }
