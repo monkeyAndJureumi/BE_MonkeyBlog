d07673e8f14ebd9d5524140c2728dcd4d15d76bc
dlwlgh9412 <dlwlgh9412@gmail.com>
2022-09-26 09:55:16 +0900

    Token레포지토리 변경

--- src/main/java/com/monkey/aggregate/user/infra/repository/TokenRepository.java
+++ src/main/java/com/monkey/aggregate/user/infra/repository/TokenRepository.java
@@ -3,10 +3,10 @@ package com.monkey.aggregate.user.infra.repository;
 import com.monkey.aggregate.user.domain.Token;
 import com.monkey.aggregate.user.domain.UserId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface TokenRepository extends MongoRepository<Token, UserId> {
+public interface TokenRepository extends CrudRepository<Token, Long> {
     Optional<Token> findByRefreshToken(String accessToken);
-    Optional<Token> findTopByUserId(UserId userId);
 }
