64745597ea57de8dd8e3ec4ab7471f019520cf35
dlwlgh9412 <dlwlgh9412@gmail.com>
2022-09-26 10:12:31 +0900

    AuthorizeCodeResponseDtot삭제

--- src/main/java/com/monkey/aggregate/user/dto/social/AuthorizeCodeResponseDto.java
+++ src/main/java/com/monkey/aggregate/user/dto/social/AuthorizeCodeResponseDto.java
@@ -1,5 +1,6 @@
 package com.monkey.aggregate.user.dto.social;
 
+@Deprecated
 public abstract class AuthorizeCodeResponseDto {
     public abstract String getCode();
     public abstract String getError();
