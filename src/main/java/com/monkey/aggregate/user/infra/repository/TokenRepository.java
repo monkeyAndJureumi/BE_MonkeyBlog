fb6f490baac5a3c3055b7634e9940e4727966ce6
dlwlgh9412 <dlwlgh9412@gmail.com>
2022-09-27 12:17:47 +0900

    몽고디비 그래들 제거

--- src/main/java/com/monkey/aggregate/user/infra/repository/TokenRepository.java
+++ src/main/java/com/monkey/aggregate/user/infra/repository/TokenRepository.java
@@ -1,8 +1,6 @@
 package com.monkey.aggregate.user.infra.repository;
 
 import com.monkey.aggregate.user.domain.Token;
-import com.monkey.aggregate.user.domain.UserId;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
