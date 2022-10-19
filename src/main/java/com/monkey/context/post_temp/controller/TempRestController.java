503911f27e559e6371495460c4ccb78c7a70d43c
dlwlgh9412 <dlwlgh9412@gmail.com>
2022-10-19 17:06:51 +0900

    임시게시글 스웨거 수정

--- src/main/java/com/monkey/context/post_temp/controller/TempRestController.java
+++ src/main/java/com/monkey/context/post_temp/controller/TempRestController.java
@@ -5,7 +5,6 @@ import com.monkey.context.post_temp.dto.PostTempSaveDto;
 import com.monkey.context.post_temp.dto.PostTempUpdateDto;
 import com.monkey.context.post_temp.service.PostTempService;
 import com.monkey.context.member.domain.MemberId;
-import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,27 +18,27 @@ import springfox.documentation.annotations.ApiIgnore;
 public class TempRestController {
     private final PostTempService postTempService;
 
-    @ApiOperation(value = "게시글 조회")
+    @ApiOperation(value = "임시 게시글 조회")
     @GetMapping("/{id}")
     public ResponseEntity<PostTempResponseDto> get(MemberId memberId, @PathVariable("id") String postId) {
         return new ResponseEntity<>(postTempService.select(memberId, postId), HttpStatus.OK);
     }
 
-    @ApiOperation(value = "게시글 생성")
+    @ApiOperation(value = "임시 게시글 생성")
     @PostMapping
     public ResponseEntity<HttpStatus> save(@RequestBody PostTempSaveDto dto) {
         postTempService.save(dto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @ApiModelProperty(value = "게시글 수정")
+    @ApiOperation(value = "임시 게시글 수정")
     @PatchMapping
     public ResponseEntity<HttpStatus> update(@RequestBody PostTempUpdateDto dto) {
         postTempService.update(dto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @ApiModelProperty(value = "게시글 삭제")
+    @ApiOperation(value = "임시 게시글 삭제")
     @DeleteMapping("/{id}")
     public ResponseEntity<HttpStatus> delete(@ApiIgnore MemberId memberId, @PathVariable("id") String id) {
         postTempService.delete(memberId, id);
